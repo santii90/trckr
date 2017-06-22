d5cb3690751deefc405fb4a3a9b8456882d4b3d6
Santiago Vazquez <svazquez@altimetrik.com>
2017-06-22 18:14:30 -0300

    major changes to strcuture, vue and feathers

---
+++ tracker/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Welcome to the Tracker</h1>
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <li v-for="value in matches">
+    {{ value }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tracker/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import * as services from './services'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  data: {
+    matches: services.match.find()
+  },
+  components: { App },
+  methods: {
+    getMatch () {
+      return services.match.find()
+    }
+  }
+})
