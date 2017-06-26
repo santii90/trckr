8d0a9f487d014caba945412b010b862248f398e1
Santiago Vazquez <svazquez@altimetrik.com>
2017-06-26 17:38:42 -0300

    heroku config

--- tracker/src/feathers.js
+++ tracker/src/feathers.js
@@ -1,20 +1,20 @@
 import rest from 'feathers-rest/client'
-// const jQuery = require('jquery')
+const jQuery = require('jquery')
 // Include and set up feathers client
 const Feathers = require('feathers/client')
 const hooks = require('feathers-hooks')
 const authentication = require('feathers-authentication-client')
 // const socketio = require('feathers-socketio/client')
 // const io = require('socket.io-client')
-var port = process.env.PORT || 3030
-var host = process.env.HOST || 'http://localhost'
+var port = 3030
+var host = location.origin.replace(/^http/, 'ws')
 var finalHost = host + ':' + port
 console.log(process.env.PORT + '   ' + process.env.HOST)
 console.log(finalHost)
 // const socket = io(finalHost)
 
 const feathers = Feathers()
-.configure(rest('https://trckrr.herokuapp.com:80').fetch(window.fetch.bind(window)))
+.configure(rest(finalHost).jquery(jQuery))
 .configure(hooks())
 .configure(authentication({storage: window.localStorage}))
 
