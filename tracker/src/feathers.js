1eab3d8b33db13ef880263dbdd15cd2fded062af
Santiago Vazquez <svazquez@altimetrik.com>
2017-06-26 13:30:02 -0300

    rest api

--- tracker/src/feathers.js
+++ tracker/src/feathers.js
@@ -1,16 +1,19 @@
+import rest from 'feathers-rest/client'
+import jQuery from 'jquery'
 // Include and set up feathers client
 const Feathers = require('feathers/client')
 const hooks = require('feathers-hooks')
 const authentication = require('feathers-authentication-client')
-const socketio = require('feathers-socketio/client')
-const io = require('socket.io-client')
+// const socketio = require('feathers-socketio/client')
+// const io = require('socket.io-client')
 var port = process.env.PORT || 3030
 var host = process.env.HOST || 'http://localhost'
 var finalHost = host + ':' + port
 console.log(finalHost)
-const socket = io(finalHost)
+// const socket = io(finalHost)
+
 const feathers = Feathers()
-.configure(socketio(socket))
+.configure(rest(finalHost).jquery(jQuery))
 .configure(hooks())
 .configure(authentication({storage: window.localStorage}))
 
