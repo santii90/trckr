1dd7b9fe12093a77a90d2e812f8d54631798a6f8
Santiago Vazquez <svazquez@altimetrik.com>
2017-06-26 12:36:47 -0300

    new port

--- tracker/src/feathers.js
+++ tracker/src/feathers.js
@@ -6,7 +6,9 @@ const socketio = require('feathers-socketio/client')
 const io = require('socket.io-client')
 var port = process.env.PORT || 3030
 var host = process.env.HOST || 'http://localhost'
-const socket = io(host + ':' + port)
+var finalHost = host + ':' + port
+console.log(finalHost)
+const socket = io(finalHost)
 const feathers = Feathers()
 .configure(socketio(socket))
 .configure(hooks())
