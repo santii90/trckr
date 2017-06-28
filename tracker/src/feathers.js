67876b4fca5470a9ae571931088e69c826ab8dfc
Santiago Vazquez <santi.v@me.com>
2017-06-27 22:52:04 -0300

    production env set

--- tracker/src/feathers.js
+++ tracker/src/feathers.js
@@ -7,12 +7,19 @@ const authentication = require('feathers-authentication-client')
 // const socketio = require('feathers-socketio/client')
 // const io = require('socket.io-client')
 // const socket = io(finalHost)
+var host = 'http://localhost:3000'
+
+if (process.env.NODE_ENV === 'production') {
+  host = 'http://138.197.91.189'
+}
 
 const feathers = Feathers()
-// .configure(rest('http://localhost:3000').jquery(jQuery))
-.configure(rest('http://138.197.91.189').jquery(jQuery))
-.configure(hooks())
-.configure(authentication({storage: window.localStorage}))
+  // .configure(rest('http://localhost:3000').jquery(jQuery))
+  .configure(rest(host).jquery(jQuery))
+  .configure(hooks())
+  .configure(authentication({
+    storage: window.localStorage
+  }))
 
 // Add services
 feathers.service('match')
